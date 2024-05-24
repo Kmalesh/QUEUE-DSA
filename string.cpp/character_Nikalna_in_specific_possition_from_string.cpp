d229d3502a18c6be992d8fa1591dc5effda0218e
kmalesh <kamleshk66492@gmail.com>
2024-05-25 00:23:19 +0530

    string function

---
+++ string.cpp/character_Nikalna_in_specific_possition_from_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name="kamalesh";
+    cout<<name.at(5);  //jis possition ka number doge wahi charecter aayega
+    
+
+}
